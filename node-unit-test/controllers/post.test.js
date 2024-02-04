0bd83c0b655b873d3282fd60573048e49f1ce325
MiguelRobles7 <51726200+MiguelRobles7@users.noreply.github.com>
2024-02-04 14:55:55 +0900

    test: add failing addPost test

--- node-unit-test/controllers/post.test.js
+++ node-unit-test/controllers/post.test.js
@@ -1,2 +1,11 @@
 import { addPost } from "./postController";
 import { jest } from "@jest/globals";
+import 'regenerator-runtime/runtime'
+
+describe("make sure addPost is running", () => {
+    it("should return 200", async () => {
+      expect(addPost.statusCode).toBe(200);
+    });
+  });
+
+  
