fc5cbddc44dcd7e9e9311d293f06419d7e6612fa
jms <74128476+jmslouis@users.noreply.github.com>
2024-02-05 17:29:40 +0800

    feat: setup mongodb atlas

--- node-unit-test/models/connection.js
+++ node-unit-test/models/connection.js
@@ -1,7 +1,7 @@
 // This file is initializing the mongodb connection
 // and exporting it for use in all other files through the module.exports
 const mongoose = require('mongoose');
-const databaseURL = 'mongodb://localhost:27017/logindb';
+const databaseURL = 'mongodb+srv://jmsballatan:04q3srM1Cx5h6A92@stsweng-test.nmywbmx.mongodb.net/logindb?retryWrites=true&w=majority';
 
 const options = { useNewUrlParser: true,
   useUnifiedTopology: true,
